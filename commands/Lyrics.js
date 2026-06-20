06fc3b2154dabb1b076442a34d0f496e6ecc3f3f
kfjr82nwmzqvhdpxl9tcb4yqrs7gjm1a <rovicpet974@gmail.com>
2026-06-20 05:34:36 +0300

    Update Lyrics.js

--- commands/Lyrics.js
+++ commands/Lyrics.js
@@ -28,54 +28,41 @@ export const commands = [
       const url = `https://api.popcat.xyz/v2/lyrics?song=${encodeURIComponent(text)}`
       const { data } = await axios.get(url)
 
-      if (data.error || !data.lyrics) {
+      if (data.error || !data.message) {
         const notFoundMsg = await t(from, 'lyrics', 'notFound')
         return sock.sendMessage(from,{
           text: notFoundMsg
         },{ quoted: msg })
       }
 
+      const song = data.message
       const userLang = getUserLang(from)
 
-      const translatedTitle = await translate(data.title, userLang)
-      const translatedArtist = await translate(data.artist, userLang)
-      
-      let lyricsText = data.lyrics || data.message || ''
-      
-      if (lyricsText.length > 3500) {
-        lyricsText = lyricsText.slice(0, 3500) + '...'
-      }
-      
-      const translatedLyrics = await translate(lyricsText, userLang)
+      const translatedTitle = await translate(song.title, userLang)
+      const translatedArtist = await translate(song.artist, userLang)
+      const translatedLyrics = await translate(song.lyrics.slice(0,3500), userLang)
 
       const resultTemplate = await t(from, 'lyrics', 'result')
       const caption = resultTemplate
         .replace('{title}', translatedTitle)
         .replace('{artist}', translatedArtist)
         .replace('{lyrics}', translatedLyrics)
-        .replace('{url}', data.url || '')
+        .replace('{url}', song.url)
 
-      const imageUrl = data.image || data.thumbnail || data.albumArt || null
-      
-      if (imageUrl) {
-        await sock.sendMessage(from,{
-          image: { url: imageUrl },
-          caption
-        },{ quoted: msg })
-      } else {
-        await sock.sendMessage(from,{
-          text: caption
-        },{ quoted: msg })
-      }
+      await sock.sendMessage(from,{
+        image:{ url: song.image },
+        caption
+      },{ quoted: msg })
 
     } catch (err) {
-      console.error('Lyrics error:', err)
+
       const errorMsg = await t(from, 'lyrics', 'error')
       sock.sendMessage(from,{
         text: errorMsg
           .replace('{botName}',botName)
           .replace('{botVersion}',botVersion)
       },{ quoted: msg })
+
     }
 
   }
